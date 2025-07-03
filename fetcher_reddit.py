71441768934551bf0611b7c56206911c1f3c17e2
Albertinos777 <stefanodibenedetto01@gmail.com>
2025-07-03 11:33:03 +0200

    Update fetcher_reddit.py

--- fetcher_reddit.py
+++ fetcher_reddit.py
@@ -85,26 +85,26 @@ def fetch_reddit_sync(limit=50, sort=None, target="reddit_all", tag=None):
                 posts = reddit.subreddit(sub).new(limit=limit * 2)
 
             for post in posts:
-               print(f"DEBUG POST: {post.title} | {post.url}")
+                print(f"DEBUG POST: {post.title} | {post.url}")
 
                 if not post.over_18 or post.is_self:
                     print("SCARTATO: non è over 18 o è un selfpost")
                     continue
-                
+
                 url = sanitize_url(post.url)
                 title = post.title.lower()
-                
+
                 if "v.redd.it" in url:
                     try:
                         url = post.media["reddit_video"]["fallback_url"]
                     except (KeyError, TypeError):
                         print("SCARTATO: no fallback url")
                         continue
-                
+
                 if not is_direct_media(url):
                     print(f"SCARTATO: non è media diretto - {url}")
                     continue
-                
+
                 if any(bad in title for bad in BANNED_WORDS):
                     print(f"SCARTATO: parola bannata trovata in {title}")
                     continue
